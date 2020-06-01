236ffe84c342d82a47abed70ee6933f50fde2188
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-05-31 20:28:37 -0600

    pages cleaned up and skeleton for my form is complete

--- assets/js/custom-functionality.js
+++ assets/js/custom-functionality.js
@@ -1,24 +1,35 @@
 const phoneIcon = document.getElementById('phoneIcon');
+const phoneIcon2 = document.getElementById('phoneIcon2');
     phoneIcon.addEventListener('mouseenter', showPhone)
     phoneIcon.addEventListener('mouseleave', hidePhone)
+    phoneIcon2.addEventListener('mouseenter', showPhone)
+    phoneIcon2.addEventListener('mouseleave', hidePhone)
 
 function showPhone() {
     document.getElementById('phoneNumber').style.display="inline-block"
+    document.getElementById('phoneNumber2').style.display="inline-block"
 }
 function hidePhone() {
     document.getElementById('phoneNumber').style.display="none" 
+    document.getElementById('phoneNumber2').style.display="none" 
 }
+
 
 
 const emailIcon = document.getElementById('emailIcon');
+const emailIcon2 = document.getElementById('emailIcon2');
     emailIcon.addEventListener('mouseenter', showEmail)
     emailIcon.addEventListener('mouseleave', hideEmail)
+    emailIcon2.addEventListener('mouseenter', showEmail)
+    emailIcon2.addEventListener('mouseleave', hideEmail)
 
 function showEmail() {
     document.getElementById('emailAddy').style.display="inline-block"
+    document.getElementById('emailAddy2').style.display="inline-block"
 }
 function hideEmail() {
     document.getElementById('emailAddy').style.display="none" 
+    document.getElementById('emailAddy2').style.display="none" 
 }
 
 function jumpToProjects() {
